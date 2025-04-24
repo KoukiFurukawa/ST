e5e8adf42e8947027baa15a1284ffbb965359001
kouki928 <furukawavo@gmail.com>
2025-04-24 14:01:59 +0900

    委任状アプリ初期画面

---
+++ ComisionApp/src/app/answer/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+
+function Answer() {
+    return (
+        <div>
+            <h1>Answer</h1>
+            <p>Management page content goes here.</p>
+            <p>More content can be added as needed.</p>
+        </div>
+    )
+}
+
+export default Answer
